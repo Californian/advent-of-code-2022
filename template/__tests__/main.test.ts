600551bc568f962f4d41d54aac70f052814f77d4
Californian <1079539+Californian@users.noreply.github.com>
2022-12-03 04:05:03 -0800

    Add solution template.

---
+++ template/__tests__/main.test.ts
@@ -0,0 +1,13 @@
+import { promises as fs } from "fs";
+
+import { } from "src";
+
+describe("", () => {
+  // Act before assertions
+  beforeAll(async () => {
+  });
+
+  it("", () => {
+    expect(true).toBeTruthy();
+  })
+});
---
+++ template/src/main.ts
@@ -0,0 +1,11 @@
+import { program } from "commander";
+import { promises as fs } from "fs";
+import { } from "src";
+
+program
+  .name("")
+  .action(async () => {
+    console.log();
+  })
+
+program.parse();
